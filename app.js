d4fd7ad9d3a93fe855b498517840854dcfd903e9
liue853224@gmail.com <liue853224@gmail.com>
2024-08-22 23:13:00 +0800

    add header & footer

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 3000
 const flash = require('connect-flash')
 const session = require('express-session')
 const SESSION_SECRET = 'secret'
+const { getUser } = require('./helpers/auth-helpers')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
@@ -22,6 +24,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
