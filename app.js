40a0dcd07ec59d1c3225e4a2071b5399f3a0c41e
liue853224@gmail.com <liue853224@gmail.com>
2024-08-22 21:40:21 +0800

    add error handler

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
