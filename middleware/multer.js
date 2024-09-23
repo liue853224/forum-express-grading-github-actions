9ea190e43ec3c354cd4e1a0a2277a9f92288a67f
liue853224@gmail.com <liue853224@gmail.com>
2024-09-23 15:26:47 +0800

    add feature for user get profile and edit profile

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,3 +1,12 @@
 const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const fs = require('fs')
+const path = require('path')
+
+// 確認 temp 資料夾存在
+const tempDir = path.join(__dirname, '..', 'temp')
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir)
+}
+
+const upload = multer({ dest: tempDir })
 module.exports = upload
